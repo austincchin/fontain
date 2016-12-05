9769afe0a7264671c19735ec4a3e42e7e7f2abb5
ebi888 <ebi888@optonline.net>
2016-12-05 12:31:15 -0500

    words print but out of order, can do individual letters without interfering with words

--- demo/font.js
+++ demo/font.js
@@ -5,11 +5,35 @@ var letterNum = document.getElementById("letter").value;
 var mesh;
 var A = {char:"A", mesh:{}, X:1, Y:1};
 var B = {char:"B", mesh:{}, X:1, Y:1};
+var C = {char:"C", mesh:{}, X:1, Y:1};
+var D = {char:"D", mesh:{}, X:1, Y:1};
+var E = {char:"E", mesh:{}, X:1, Y:1};
+var F = {char:"F", mesh:{}, X:1, Y:1};
 var G = {char:"G", mesh:{}, X:1, Y:1};
+var H = {char:"H", mesh:{}, X:1, Y:1};
+var I = {char:"I", mesh:{}, X:1, Y:1};
+var J = {char:"J", mesh:{}, X:1, Y:1};
+var K = {char:"K", mesh:{}, X:1, Y:1};
+var L = {char:"L", mesh:{}, X:1, Y:1};
+var M = {char:"M", mesh:{}, X:1, Y:1};
+var N = {char:"N", mesh:{}, X:1, Y:1};
+var O = {char:"O", mesh:{}, X:1, Y:1};
+var P = {char:"P", mesh:{}, X:1, Y:1};
 var Q = {char:"Q", mesh:{}, X:1, Y:1};
-var letters = [A, B, G, Q];
+var R = {char:"R", mesh:{}, X:1, Y:1};
+var S = {char:"S", mesh:{}, X:1, Y:1};
+var T = {char:"T", mesh:{}, X:1, Y:1};
+var U = {char:"U", mesh:{}, X:1, Y:1};
+var V = {char:"V", mesh:{}, X:1, Y:1};
+var W = {char:"W", mesh:{}, X:1, Y:1};
+var X = {char:"X", mesh:{}, X:1, Y:1};
+var Y = {char:"Y", mesh:{}, X:1, Y:1};
+var Z = {char:"Z", mesh:{}, X:1, Y:1};
+var letters = [A, B, C, D, E, F, G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
 var path = 'demo/svg/singleChar/' + letters[letterNum].char + '.svg';
 var word = "";//AGGQ";
+var wInput = "";
+var wList = [];
 
 const fs = require('fs');
 var extractPath = require('extract-svg-path').parse;
@@ -51,8 +75,10 @@ document.getElementById("sliderY").onchange = function(event) {
 
 document.getElementById("letter").onchange = function(event) {
     letterNum = event.target.value;
+    word = "";
     path = 'demo/svg/singleChar/' + letters[letterNum].char + '.svg';
     //console.log(path);
+    //word = word + letters[letterNum].char;
     loadSvg(path, function (err, svg) {
       if (err) throw err;
       var svgPath = extractPath(svg);
@@ -64,40 +90,60 @@ document.getElementById("letter").onchange = function(event) {
     })
 };
 
+document.getElementById("wordInput").onchange = function(event) {
+    wInput = event.target.value;
+    word = "";
+    var index = 0;
+    while (index < wInput.length){
+      var cNum = wInput.charCodeAt(index)-97;
+
+      if (cNum >= 0 && cNum <= 25){
+        word += letters[cNum].char;
+      }
+      index += 1;
+    }
+    console.log(word);
+    render();
+
+};
+
 function render () {
   context.clearRect(0, 0, size, size);
   context.save();
-  console.log(letters);
+  //console.log(letters);
 
   var center;
   if (word.length>0) {
     var i = 0;
-    var scale = size / (word.length*2);
-    while (i<word.length) {
-      console.log("hey");      
+    var scale = size / (word.length*3); //scale to fit onto page
+    var offset = 0;
+    while (i<word.length) {             //iterate through word
+      console.log("hey, i = " + i + "/" + (word.length-1));      
       var temp;
-      var j = 0;
       var num;
-      while(j<letters.length) {
-        if (letters[j].char == word[i]) {
-          num = j;
-          console.log(num);
-          break;
-        }
-        j++;
-      }
+      num = word.charCodeAt(i)-65;
+      console.log(num + " (" + letters[num].char + ")");
+
+      
+      
       loadSvg('demo/svg/singleChar/' + word[i] + '.svg', function (err, svg) {
+        var html = await getRandomPonyFooArticle();
+        console.log("index: " + i);
         if (err) throw err
-        console.log(i);
+        //console.log(i);
         var svgPath = extractPath(svg);
         temp = createMesh(svgPath, {
           scale: 1,
           simplify: 0.01
         })
+
+        var x_trans = (512/word.length-50)+((130-(4*word.length))*offset);
+
         console.log(i);
         console.log(temp);
         context.save();
-        context.translate(((size/2)+20*i), (size/2));
+        context.translate(x_trans, (size/2));
+        console.log("translated " + word[offset] +  " to x: " + x_trans);
         context.scale(scale/letters[num].X, -scale/letters[num].Y);
         context.beginPath();
         context.lineJoin = 'round';
@@ -105,11 +151,13 @@ function render () {
         context.lineWidth = 2 / scale;
         drawTriangles(context, temp.positions, temp.cells);
         context.fillStyle = '#000000';
-        context.strokeStyle = '#3b3b3b';
+        context.strokeStyle = '#000000';
         context.fill();
         context.stroke();
         context.restore();
+        offset += 1;
       });
+
       i++;
       }
   }
@@ -124,7 +172,7 @@ function render () {
     context.lineWidth = 2 / scale;
     drawTriangles(context, mesh.positions, mesh.cells);
     context.fillStyle = '#000000';
-    context.strokeStyle = '#3b3b3b';
+    context.strokeStyle = '#000000';
     context.fill();
     context.stroke();
     context.restore();
